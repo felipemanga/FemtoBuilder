47caf555f6a7dcbb4284c80d85ebf7fe41cd0719
FManga <lufeboma@gmail.com>
2022-08-06 08:10:33 +0000

    Fix shell function always returning -1

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -4,21 +4,24 @@
 #include <ios>
 #include <iostream>
 #include <stdio.h>
+#include <memory>
 
 int shell(const std::string& cmd) {
   int result = -1;
-  auto onClose = [&](FILE* file) {
-    result = pclose(file);
-    result = WEXITSTATUS(result);
-  };
-  std::unique_ptr<FILE, decltype(onClose)> pipe(popen(cmd.c_str(), "r"), onClose);
-  if (pipe) {
-    std::cout << std::unitbuf;
-    char buffer[2];
-    while (fgets(buffer, 2, pipe.get()) != nullptr) {
-      std::cout << buffer;
-    }
-    std::cout << std::nounitbuf;
+  {
+      auto onClose = [&](FILE* file) {
+          result = pclose(file);
+          result = WEXITSTATUS(result);
+      };
+      std::unique_ptr<FILE, decltype(onClose)> pipe(popen(cmd.c_str(), "r"), onClose);
+      if (pipe) {
+          std::cout << std::unitbuf;
+          char buffer[2];
+          while (fgets(buffer, 2, pipe.get()) != nullptr) {
+              std::cout << buffer;
+          }
+          std::cout << std::nounitbuf;
+      }
   }
   return result;
 }
